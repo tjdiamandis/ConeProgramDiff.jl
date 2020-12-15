a2069acb48c5ded323fe8575afe31f9be829dfd2
Theo Diamandis <tjdiamandis@gmail.com>
2020-12-14 21:51:32 -0500

    tweaks here and there

--- src/cone_solve.jl
+++ src/cone_solve.jl
@@ -6,8 +6,9 @@ const SUPPORTED_SOLVERS = Dict(
 
 # TODO: Type function params A,b,c
 function solve_and_diff(
-    A, b, c, cone_prod::Vector{T}; warm_start=nothing, solver="SCS"
+    A, b, c, cone_prod::Vector{T}; kwargs...
 ) where {T <: MOI.AbstractVectorSet}
+    kwargs = Dict(kwargs)
     m,n = size(A)
 
     # Arguments check
@@ -15,45 +16,50 @@ function solve_and_diff(
     all([typeof(c) <: SUPPORTED_INPUT_SETS for c in cone_prod]) || throw(ArgumentError("Unsupported cones"))
     all([MOI.dimension(c) > 0 for c in cone_prod]) || throw(ArgumentError("Cones must have nonzero dimension"))
     sum([MOI.dimension(c) for c in cone_prod]) == m || throw(DimensionMismatch("Mismatch between dimension of c and cone"))
-    if solver in keys(SUPPORTED_SOLVERS)
+    if haskey(kwargs, :solver) && kwargs[:solver] in keys(SUPPORTED_SOLVERS)
         optimizer = SUPPORTED_SOLVERS[solver]
         # TODO: check cone_prod for each solver
-    else
+    elseif haskey(kwargs, :solver)
         throw(ArgumentError("Invalid solver"))
+    else
+        optimizer = SCS.Optimizer
     end
 
-    return _solve_and_diff(A, b, c, cone_prod, warm_start, optimizer)
+    # TODO: add warm start??
+    # TODO: add lsqr option
+    return _solve_and_diff(A, b, c, cone_prod, nothing, optimizer, false)
 end
 
-function _solve_and_diff(A, b, c, cone_prod, warm_start, optimizer)
+function _solve_and_diff(A, b, c, cone_prod, warm_start, optimizer, use_lsqr)
     m,n = size(A)
+    typeof(A) <: SparseMatrixCSC && dropzeros!(A)
     x_star, y_star, s_star = solve_opt_problem(A, b, c, cone_prod, warm_start, optimizer)
 
     Q = spzeros(m+n+1,m+n+1)
-    Q[1:n,n+1:n+m]      = A'
-    Q[n+1:n+m,1:n]      = -A
-    Q[n+1:n+m,m+n+1]    = b
-    Q[m+n+1,n+1:n+m]    = -b'
-    Q[1:n,m+n+1]        = c
-    Q[m+n+1,1:n]        = -c'
+    Q[1:n,n+1:n+m]      .= A'
+    Q[n+1:n+m,1:n]      .= -A
+    Q[n+1:n+m,m+n+1]    .= b
+    Q[m+n+1,n+1:n+m]    .= -b
+    Q[1:n,m+n+1]        .= c
+    Q[m+n+1,1:n]        .= -c
 
-    DπKdual_v = d_project_onto_cone(y_star - s_star, cone_prod)
-    println(DπKdual_v)
+    u, v, w = (x_star, y_star - s_star, 1.0)
+    DπKdual_v = d_project_onto_cone(v, [MOI.dual_set(c) for c in cone_prod])
 
-    # TODO: extend to dy and ds
-    function pullback(dx, dy, ds)
-        u, v, w = (x_star, y_star - s_star, 1.0) #def z
+    function pullback(dx)
+        # Assume that dy = ds = 0
         dz = [
             dx;
-            DπKdual_v'*(dy + ds) - ds;
-            -x_star' * dx - y_star' * dy - s_star' * ds
+            zeros(length(y_star));
+            -x_star' * dx
         ]
         Pi_z = pi_z(u, v, w, cone_prod)
 
-        M = (Q - I) * dpi(u, v, w, cone_prod) + I
-        g = lsqr(-M', dz)
-        dQ = g * Pi_z'
+        M = (Q - I) * dpi_z(u, v, w, cone_prod) + I
+        g = use_lsqr ? lsqr(-M', dz, atol=1e-10, btol=1e-10) : -M' \ dz
 
+        # TODO: make more efficient by only pulling needed cols/rows
+        dQ = g * pi_z(u, v, w, cone_prod)'
         dA = dQ[1:n,n+1:n+m]'  - dQ[n+1:n+m,1:n]    # dQ12' - dQ21
         db = dQ[n+1:n+m,m+n+1] - dQ[m+n+1,n+1:n+m]  # dQ23 - dQ32'
         dc = dQ[1:n,m+n+1]     - dQ[m+n+1,1:n]      # dQ13 - dQ31'
@@ -61,18 +67,19 @@ function _solve_and_diff(A, b, c, cone_prod, warm_start, optimizer)
     end
 
     function pushforward(dA, db, dc)
-        u, v, w = (x_star, y_star - s_star, 1.0)
         dQ = spzeros(n+m+1, n+m+1)
-        dQ[1:n,n+1:n+m]      = dA'
-        dQ[n+1:n+m,1:n]      = -dA
-        dQ[n+1:n+m,m+n+1]    = db
-        dQ[m+n+1,n+1:n+m]    = -db'
-        dQ[1:n,m+n+1]        = dc
-        dQ[m+n+1,1:n]        = -dc'
-        M = (Q - I) * dpi(u, v, w, cone_prod) + I
+        dQ[1:n,n+1:n+m]      .= dA'
+        dQ[n+1:n+m,1:n]      .= -dA
+        dQ[n+1:n+m,m+n+1]    .= db
+        dQ[m+n+1,n+1:n+m]    .= -db #dont use tranpose bc of broadcast
+        dQ[1:n,m+n+1]        .= dc
+        dQ[m+n+1,1:n]        .= -dc
+
+        M = (Q - I) * dpi_z(u, v, w, cone_prod) + I
         g = dQ*pi_z(u, v, w, cone_prod)
-        dz = lsqr(-M, g)
-        du, dv, dw = dz[1:n], dz[n+1:n+m], dz[n+m+1]
+        # dz = -M \ g
+        dz = lsqr(-M, g, atol=1e-10, btol=1e-10)
+        @views du, dv, dw = dz[1:n], dz[n+1:n+m], dz[n+m+1]
         dx = du .- dw*x_star
         dy = DπKdual_v*dv - dw*y_star
         ds = DπKdual_v*dv - dv - dw*s_star
@@ -83,7 +90,8 @@ end
 
 function solve_opt_problem(A, b, c, cone_prod, warm_start, optimizer_factory)
     m,n = size(A)
-    model = Model(optimizer_factory)
+    model = Model()
+    set_optimizer(model, optimizer_with_attributes(SCS.Optimizer, "eps" => 1e-10, "max_iters" => 100000, "verbose" => 0))
     @variable(model, x[1:n])
     @variable(model, s[1:m])
     @objective(model, Min, c'*x)
@@ -94,28 +102,34 @@ function solve_opt_problem(A, b, c, cone_prod, warm_start, optimizer_factory)
         curr += cone.dimension
     end
     optimize!(model)
-    if ~(termination_status(model) == MOI.OPTIMAL)
+    if ~(termination_status(model) in [MOI.OPTIMAL, MOI.ALMOST_OPTIMAL])
         error("Model not solved correctly.
                Termination status: $(termination_status(model))")
+    end
+    if termination_status(model) ~= MOI.OPTIMAL
+        @warn "Maximum iterations reached. Set 'max_iters' to increase accuracy"
     end
 
     # primal_status(model)
     # dual_status(model)
-    return value.(x), dual.(con), value.(s)
+    y = isapprox(A'* dual.(con) + c, zeros(length(c)),atol=1e-6) ? dual.(con) : -dual.(con)
+    return value.(x), y, value.(s)
 end
 
-function pi_z(u, v, w, cone_prod)
+function pi_z(u, v, w, cone_prod; dual=false)
+    cone = dual ? [MOI.dual_set(c) for c in cone_prod] : cone_prod
     return vcat(
         u,
-        project_onto_cone(v, [MOI.dual_set(c) for c in cone_prod]),
+        project_onto_cone(v, cone),
         max(w, 0.0)
     )
 end
 
-function dpi(u, v, w, cone_prod)
+function dpi_z(u, v, w, cone_prod; dual=false)
+    cone = dual ? [MOI.dual_set(c) for c in cone_prod] : cone_prod
     return blockdiag(
         sparse(Matrix{Float64}(I, length(u), length(u))),
-        d_project_onto_cone(v, [MOI.dual_set(c) for c in cone_prod]),
+        d_project_onto_cone(v, cone),
         sparse(Matrix{Float64}(I, 1,1))
     )
 end
