239becbba1b1b56ec6dc06ca73f71bf6f28e325a
Theo Diamandis <tjdiamandis@gmail.com>
2020-12-14 21:53:58 -0500

    change file name

--- src/cones.jl
+++ src/cones.jl
@@ -58,10 +58,25 @@ end
 function _proj_custom(x, cone)
     error("Not Implemented")
     # TODO: Exponential Cone:
-    # Projection: https://web.stanford.edu/~boyd/papers/pdf/prox_algs.pdf 6.3
-    # TODO: Power cone
-    # https://link.springer.com/article/10.1007/s00186-015-0514-0
+    if cone <: MOI.ExponentialCone
+        # X = (r, s, t)
+        # Projection: https://web.stanford.edu/~boyd/papers/pdf/prox_algs.pdf 6.3
+        # if x is in exponential cone: return x
+        # elif -x is in exponential cone: return 0
+        # elif first two coordinates are less than zero, return (r, 0, max(t, 0))
+        # otherwise, solve constrained optimization
+        continue
+    elseif typeof(cone) <: MOI.PowerCone
+        # TODO: Power cone
+        # https://link.springer.com/article/10.1007/s00186-015-0514-0
+        # or http://www.optimization-online.org/DB_FILE/2014/08/4502.pdf (Proposition 2.3)
+        # if x is in power cone: return x
+        # elif -x is in power cone: return -x
+        # elif z == 0: return (max(x, 0), max(y, 0), 0)
+        # otherwise, solve constrained optimization
+    end
 end
+
 
 # TODO: can probably write this code more efficiently? lots of repeats
 function d_project_onto_cone(x, cone_prod)
@@ -90,11 +105,20 @@ end
 
 function _d_proj_custom(x, cone)
     error("Not Implemented")
-    # TODO: Exponential Cone:
-    # Grad: http://proceedings.mlr.press/v70/ali17a/ali17a.pdf lemma 3.6
-    # TODO: Power cone
-    # https://link.springer.com/article/10.1007/s00186-015-0514-0
+    if cone <: MOI.ExponentialCone
+        # TODO: Exponential Cone:
+        # Grad: http://proceedings.mlr.press/v70/ali17a/ali17a.pdf lemma 3.6
+        # Supplement: https://arxiv.org/pdf/1705.00772.pdf
+        continue
+    elseif cone <: MOI.PowerCone
+        # TODO: Power cone
+        # x = (x, y, z) ∈ R × R × R^n
+        # https://link.springer.com/article/10.1007/s00186-015-0514-0
+        # or http://www.optimization-online.org/DB_FILE/2014/08/4502.pdf (Theorem 3.1)
+        continue
+    end
 end
+
 
 function MOSD.projection_gradient_on_set(::MOSD.DefaultDistance, v::AbstractVector{T}, ::MOI.Reals) where {T}
     return ones(T, (length(v), length(v)))
