9c0e68bafd7b8252c8720925681853ad006cb8eb
Theo Diamandis <tjdiamandis@gmail.com>
2020-12-16 00:03:21 -0500

    dProj for exp cone

--- src/cones.jl
+++ src/cones.jl
@@ -29,7 +29,7 @@ const SUPPORTED_INPUT_SETS = Union{
     SUPPORTED_NON_MOSD_INPUT_SETS
 }
 
-const EXP_CONE_THRESH = 1e-6
+const EXP_CONE_THRESH = 1e-10
 
 
 function project_onto_cone(x, cone_prod)
@@ -72,16 +72,12 @@ function _proj_custom(x, cone)
 end
 
 
-function MOSD.projection_gradient_on_set(::MOSD.DefaultDistance, v::AbstractVector{T}, ::MOI.ExponentialCone) where {T}
-    if v in
-end
-
-
 function _proj_exp_cone(v; dual=false)
     # v has dimension 3
     # Moreau’s decomposition theorem:
     #   v = P_K(v) + P_Ko(v) where K is primal cone and Ko is polar cone = -K*
     #   Dual cone proj: P_K*(v) = -P_Ko(-v)
+    # See section 6.3 of https://web.stanford.edu/~boyd/papers/pdf/prox_algs.pdf
     v = dual ? -v : v
 
     if in_exp_cone(v)
@@ -89,7 +85,7 @@ function _proj_exp_cone(v; dual=false)
     elseif in_exp_cone_dual(v)
         return dual ? -v : zeros(3)
     elseif v[1] <= 0 && v[2] <= 0 #TODO: threshold here??
-        return dual ? -[max(-v[1],0); v[2]; 0.0] : [v[1]; 0.0; max(v[3],0)]
+        return dual ? [0.0; -v[2]; max(-v[3],0)] : [v[1]; 0.0; max(v[3],0)]
     else
         return get_exp_proj_case4(v; dual=dual)
     end
@@ -113,42 +109,26 @@ end
 
 
 function get_exp_proj_case4(v; dual=false)
-    # Bisection method for root finding of h(rho) from
+    # Bisection method for root finding of h(x) from
     # https://docs.mosek.com/slides/2018/ismp2018/ismp-friberg.pdf
-    # h(x) is smooth, strictly increasing, and changes sign
+    # Thm: h(x) is smooth, strictly increasing, and changes sign
     @views r, s, t = v[1], v[2], v[3]
 
     # Note: these won't both be infinity (by case 3)
-    # TODO: figure out a better replacement for UB
+    # TODO: figure out a better replacement for Inf
     lb = r > 0 ? 1 - s/r : -1e6
     ub = s > 0 ? r/s : 1e6
 
     # h(x) = ((x-1)*r + s)/(x^2 - x + 1) * exp(x) - (r - x*s)/(x^2 - x + 1)*exp(-x) - t
     h(x) = (((x-1)*r + s) * exp(x) - (r - x*s)*exp(-x))/(x^2 - x + 1) - t
 
-    rho = find_zero(h, (lb, ub), verbose=false)
-    vp = ((rho - 1)*r + s)/(rho^2 - rho + 1) * [rho; 1.0; exp(rho)]
-    vd = (r - rho*s)/(rho^2 - rho + 1) * [1.0; 1.0-rho; -exp(-rho)]
-
-    return dual ? -vd : vp
+    # TODO: anything more efficient than bisection search?
+    x = find_zero(h, (lb, ub), verbose=false)
+    vp = ((x - 1)*r + s)/(x^2 - x + 1) * [x; 1.0; exp(x)]
+    vd = (r - x*s)/(x^2 - x + 1) * [1.0; 1.0-x; -exp(-x)]
+# -vd
+    return dual ? -v + vp : vp
 end
-
-
-# function get_exp_proj_case4(v)
-#     # v = [r; s; t]
-#     # minimize f = 1/2||x - v||^2
-#     # st       g = se^{r/s} - t = 0, s > 0
-#     # Solve with primal dual Newton method
-#     #   (https://web.stanford.edu/~boyd/papers/pdf/prox_algs.pdf 6.3)
-#     ∇g(r,s,t) = [exp(r/s), exp(r/s)*(1-r/s); -1.0]
-#     hess_g(r,s,t) = exp(r/s) * [1.0/s  -r/s^2  0;
-#                                 -r/s^2 r^2/s^3 0;
-#                                 0      0       0]
-#     # TODO: init x and λ
-#     dxdλ = [I + λ*hess_g(x...) ∇(g...); ∇g(x...)' 0] \ [x - v + λ∇g(x...); x[2]*exp(x[1]/x[2]) - x[3]]
-#
-#
-# end
 
 
 # TODO: can probably write this code more efficiently? lots of repeats
@@ -177,19 +157,55 @@ end
 
 
 function _d_proj_custom(x, cone)
-    error("Not Implemented")
-    if cone <: MOI.ExponentialCone
-        # TODO: Exponential Cone:
-        # Grad: http://proceedings.mlr.press/v70/ali17a/ali17a.pdf lemma 3.6
-        # Supplement: https://arxiv.org/pdf/1705.00772.pdf
+    if typeof(cone) <: MOI.ExponentialCone
+        _d_proj_exp_cone(x; dual=false)
+    elseif typeof(cone) <: MOI.DualExponentialCone
+        _d_proj_exp_cone(x; dual=true)
 
-    elseif cone <: MOI.PowerCone
-        # TODO: Power cone
-        # x = (x, y, z) ∈ R × R × R^n
-        # https://link.springer.com/article/10.1007/s00186-015-0514-0
-        # or http://www.optimization-online.org/DB_FILE/2014/08/4502.pdf (Theorem 3.1)
-
+    # TODO: Power cone
+    elseif typeof(cone) <: MOI.PowerCone
+        error("Not Implemented")
+    elseif typeof(cone) <: MOI.DualPowerCone
+        error("Not Implemented")
     end
+end
+
+
+function _d_proj_exp_cone(v; dual=false)
+    # Lemma 3.6 https://arxiv.org/pdf/1705.00772.pdf
+    # J_PK*(v) = I - J_PK(-v)
+    v = dual ? -v : v
+    Ip(z) = z >= 0 ? 1 : 0
+
+    if in_exp_cone(v)
+        return dual ? zeros(3,3) : Matrix{Float64}(I, 3, 3)
+    elseif in_exp_cone_dual(v)
+        return dual ? 2*Matrix{Float64}(I, 3, 3) : -Matrix{Float64}(I, 3, 3)
+    elseif v[1] <= 0 && v[2] <= 0 #TODO: threshold here??
+        vp = diagm([1; Ip(v[2]); Ip(v[3])])
+        return dual ? I - vp : vp
+    else
+        # Have to look at KKT conditions of projection problem
+        return get_exp_d_proj_case4(v; dual=dual)
+    end
+end
+
+
+function get_exp_d_proj_case4(v; dual=false)
+    Pv = get_exp_proj_case4(v, dual=false)
+    nu = Pv[3] - v[3]
+    z1,z2,z3 = Pv
+    abs(Pv[2]) <= 1e-8 && error("Numerical error in projection onto exp cone")
+    rs = z1/z2
+    exp_rs = exp(rs)
+
+    mat = inv([
+        1+nu*exp_rs/z2     -nu*exp_rs*rs/z2       0     exp_rs;
+        -nu*exp_rs*rs/z2   1+nu*exp_rs*rs^2/z2    0     (1-rs)*exp_rs;
+        0                  0                      1     -1
+        exp_rs             (1-rs)*exp_rs          -1    0
+    ])
+    return @view(mat[1:3,1:3])
 end
 
 
