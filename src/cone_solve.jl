48a8e9e17107a3784c3c370b7605b1c4d6a671a2
Chandler Squires <csquires@mit.edu>
2020-12-16 20:38:32 -0500

    change to use MOI.dimension

--- src/cone_solve.jl
+++ src/cone_solve.jl
@@ -112,13 +112,7 @@ function solve_opt_problem(A, b, c, cone_prod, warm_start, optimizer_factory)
     con = @constraint(model, A*x + s .== b)
     curr = 1
     for cone in cone_prod
-        if (typeof(cone) <: MOI.PositiveSemidefiniteConeTriangle)
-            dimension = Int64(cone.side_dimension * (cone.side_dimension + 1) / 2)
-        elseif (typeof(cone) <: MOI.ExponentialCone)
-            dimension = 3
-        else
-            dimension = cone.dimension
-        end
+        dimension = MOI.dimension(cone)
         @constraint(model, s[curr:curr+dimension-1] in cone)
         curr += dimension
     end
