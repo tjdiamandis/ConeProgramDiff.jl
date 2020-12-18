ef99aa82eb6549ba1a232fd48c72405289078085
Theo Diamandis <tjdiamandis@gmail.com>
2020-12-18 02:15:07 -0500

    test solve_and_diff

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,13 @@
 using ConeProgramDiff
 using Test
+using LinearAlgebra, SparseArrays                       #stdlib
+using JuMP, MathOptInterface, MathOptSetDistances       #Opt utils
+using IterativeSolvers, Roots                           #Eq solvers
+using SCS, ECOS, Hypatia                                #Opt solvers
+using Random
 
-using MathOptInterface
 const MOI = MathOptInterface
-# const MOSD = MathOptSetDistances
+const MOSD = MathOptSetDistances
 
 @testset "ConeProgramDiff.jl" begin
     # Write your tests here.
@@ -11,4 +15,12 @@ end
 
 @testset "Projections" begin
     include("projections.jl")
+end
+
+@testset "D_Projections" begin
+    include("d_projections.jl")
+end
+
+@testset "Solve and diff" begin
+    include("solve_and_diff.jl")
 end
---
+++ test/solve_and_diff.jl
@@ -0,0 +1,57 @@
+function check_kkt(A, b, c, cone_prod, x, y, s; tol=1e-8)
+    @assert isapprox(A*x + s, b,atol=tol)
+    @assert isapprox(A'*y + c, zeros(length(c)),atol=tol)
+    @assert isapprox(s, project_onto_cone(s, cone_prod),atol=tol)
+    @assert isapprox(y, project_onto_cone(y, [MOI.dual_set(c) for c in cone_prod]),atol=tol)
+    @assert isapprox(s'*y, 0, atol=tol)
+end
+
+
+function check_adjoint(tol)
+    Random.seed!(0)
+    dims = (15, 10)
+    A, b, c, cone_prod = l1_minimization_program(dims; solve=false)
+    x, y, s, pf, pb = solve_and_diff(A, b, c, cone_prod)
+    check_kkt(A, b, c, cone_prod, x, y, s, tol=1e-8)
+
+
+    # Check adjoint
+    pstar = dot(x, c)
+    dA, db, dc = pb(c)
+    del = 1e-6
+    x_pert, _, _, _, _ = solve_and_diff(A+del*dA, b+del*db, c+del*dc, cone_prod)
+    pstar_pert = dot(x_pert, c)
+    df = pstar_pert - pstar
+    dp = del * (sum(dA .* dA) + db'*db + dc'*dc)
+    if !isapprox(df, dp, atol=tol)
+        error("Adjoint mismatch: $(abs(df-dp))\n\tdf = $df\n\tdp = $dp")
+    end
+    return true
+end
+
+
+function check_derivative()
+    Random.seed!(0)
+    dims = (15, 10)
+    A, b, c, cone_prod = l1_minimization_program(dims; solve=false)
+    x, y, s, pf, pb = solve_and_diff(A, b, c, cone_prod)
+    check_kkt(A, b, c, cone_prod, x, y, s, tol=1e-6)
+
+    # Check derivative
+    del = 1e-6
+    dA, db, dc = del*randn(size(A)), del*randn(size(b)), del*randn(size(c))
+    dx, dy, ds = pf(dA, db, dc)
+    x_pert, y_pert, s_pert, _, _ = solve_and_diff(A+dA, b+db, c+dc, cone_prod)
+
+    # TODO: tols are high here -- should investigate
+    @assert isapprox(x_pert - x, dx, atol=1e-4)
+    @assert isapprox(y_pert - y, dy, atol=1e-4)
+    @assert isapprox(s_pert - s, ds, atol=2e-4)
+    return true
+end
+
+
+@testset "solve_and_diff Tests" begin
+    @test check_adjoint(2e-5)
+    @test check_derivative()
+end
