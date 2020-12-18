f046c6967cb57ae6f86c03e9c29d6b4fc05f48d3
Theo Diamandis <tjdiamandis@gmail.com>
2020-12-18 13:57:44 -0500

    scs interface support

--- src/cone_solve.jl
+++ src/cone_solve.jl
@@ -1,10 +1,24 @@
+const CONE_ORDER = Dict(
+    MOI.Zeros=>1,
+    MOI.Nonnegatives=>2,
+    MOI.SecondOrderCone=>3,
+    MOI.PositiveSemidefiniteConeTriangle=>4,
+    MOI.ExponentialCone=>5,
+    MOI.DualExponentialCone=>6,
+    MOI.PowerCone=>7
+)
+
 const SUPPORTED_SOLVERS = Dict(
     "ECOS" => ECOS.Optimizer,
     "Hypatia" => Hypatia.Optimizer,
     "SCS" => SCS.Optimizer
 )
 
-# TODO: Type function params A,b,c
+
+# TODO: specify for each solver
+SOLVER_OPTION_SET = [:max_iters, :eps, :warm_start, :scale, :verbose]
+
+
 function solve_and_diff(
     A::AbstractMatrix{S}, b::AbstractVector{S}, c::Vector{S}, cone_prod::Vector{T}; kwargs...
 ) where {T <: MOI.AbstractVectorSet, S <: AbstractFloat}
@@ -17,7 +31,7 @@ function solve_and_diff(
     all([MOI.dimension(c) > 0 for c in cone_prod]) || throw(ArgumentError("Cones must have nonzero dimension"))
     sum([MOI.dimension(c) for c in cone_prod]) == m || throw(DimensionMismatch("Mismatch between dimension of c and cone"))
     if haskey(kwargs, :solver) && kwargs[:solver] in keys(SUPPORTED_SOLVERS)
-        optimizer = SUPPORTED_SOLVERS[solver]
+        optimizer = SUPPORTED_SOLVERS[kwargs[:solver]]
         # TODO: check cone_prod for each solver
     elseif haskey(kwargs, :solver)
         throw(ArgumentError("Invalid solver"))
@@ -32,14 +46,22 @@ function solve_and_diff(
         use_lsqr = true
     end
 
-    return _solve_and_diff(A, b, c, cone_prod, warm_start, optimizer, use_lsqr)
+    options = Dict()
+    for option in SOLVER_OPTION_SET
+        if haskey(kwargs, option)
+            options[option] = kwargs[option]
+        end
+    end
+    options[:verbose] = haskey(options, :verbose) ? options[:verbose] : false
+
+    return _solve_and_diff(A, b, c, cone_prod, optimizer, use_lsqr; options...)
 end
 
 
-function _solve_and_diff(A, b, c, cone_prod, warm_start, optimizer, use_lsqr)
+function _solve_and_diff(A, b, c, cone_prod, optimizer, use_lsqr; options...)
     m,n = size(A)
     typeof(A) <: SparseMatrixCSC && dropzeros!(A)
-    x_star, y_star, s_star, sol = solve_opt_problem(A, b, c, cone_prod, warm_start, optimizer)
+    x_star, y_star, s_star = solve_opt_problem(A, b, c, cone_prod, optimizer; options...)
 
     Q = spzeros(m+n+1,m+n+1)
     Q[1:n,n+1:n+m]      .= A'
@@ -49,6 +71,7 @@ function _solve_and_diff(A, b, c, cone_prod, warm_start, optimizer, use_lsqr)
     Q[1:n,m+n+1]        .= c
     Q[m+n+1,1:n]        .= -c
 
+    # precompute M
     u, v, w = (x_star, y_star - s_star, 1.0)
     M = (Q - I) * dpi_z(u, v, w, cone_prod) + I
 
@@ -91,26 +114,18 @@ function _solve_and_diff(A, b, c, cone_prod, warm_start, optimizer, use_lsqr)
         ds = DπKdual_v*dv - dv - dw*s_star
         return dx, dy, ds
     end
-    return x_star, y_star, s_star, pushforward, pullback, sol
+
+    return x_star, y_star, s_star, pushforward, pullback
 end
 
-cone_order = Dict(
-    MOI.Zeros=>1,
-    MOI.Nonnegatives=>2,
-    MOI.SecondOrderCone=>3,
-    MOI.PositiveSemidefiniteConeTriangle=>4,
-    MOI.ExponentialCone=>5,
-    MOI.DualExponentialCone=>6,
-    MOI.PowerCone=>7
-)
 
 function reorder_opt_problem_scs(A, b, cones)
     # sort the cones so that they're in the correct order
-    sort_ixs = sortperm(cones, by=cone->cone_order[typeof(cone)])
+    sort_ixs = sortperm(cones, by=cone->CONE_ORDER[typeof(cone)])
     sorted_cones = cones[sort_ixs]
 
     f = 0  # the number of zero cones
-    l = 0  # the number of linear cones
+    l = 0  # the number of nonneg cones
     q = Int[]  # the array of SOC sizes
     s = Int[]  # the array of SDC sizes
     ep = 0  # the number of exponential cones
@@ -149,12 +164,13 @@ function reorder_opt_problem_scs(A, b, cones)
 
     index_map = [original_ixs[i] for i in sort_ixs]
     index_map = collect(Iterators.flatten(index_map))
+    reverse_map = sortperm(index_map)
 
-    return A[index_map, :], b[index_map], cone_dict
+    return A[index_map, :], b[index_map], cone_dict, reverse_map
 end
 
 
-function solve_opt_problem(A, b, c, cone_prod, warm_start, optimizer_factory)
+function solve_opt_problem(A, b, c, cone_prod, optimizer_factory; options...)
     # TODO: potentially play with the scale depending on the cones?
     #       e.g. see https://github.com/jump-dev/Convex.jl/issues/104
     #       Essentially want to have the primal and dual res down at the same rate
@@ -162,15 +178,48 @@ function solve_opt_problem(A, b, c, cone_prod, warm_start, optimizer_factory)
     #           dual slower -> dec. scale
     m,n = size(A)
 
-    A_reordered, b_reordered, cone_dict = reorder_opt_problem_scs(A, b, cone_prod)
-    f, l, q, s = cone_dict[:f], cone_dict[:l], cone_dict[:q], cone_dict[:s]
-    ep, ed, p = cone_dict[:ep], cone_dict[:ed], cone_dict[:p]
-    sol = SCS_solve(SCS.DirectSolver, m, n, A_reordered, b_reordered, c, f, l, q, s, ep, ed, p)
-    println(sol)
+    if optimizer_factory == SCS.Optimizer
+        A_reordered, b_reordered, cone_dict, reverse_map = reorder_opt_problem_scs(A, b, cone_prod)
+        f, l, q, s = cone_dict[:f], cone_dict[:l], cone_dict[:q], cone_dict[:s]
+        ep, ed, p = cone_dict[:ep], cone_dict[:ed], cone_dict[:p]
 
+        # TODO: clean up argument passing
+        # TODO: for some reason this fails when b is a sparse vector -- see SCS.jl
+        scs_sol = SCS_solve(SCS.IndirectSolver, m, n,
+                            A_reordered, b_reordered, c,
+                            f, l, q, s, ep, ed, p;
+                            options...)
+        # TODO: additional solution checks
+        #/* SCS returns one of the following integers: */
+        #define SCS_INFEASIBLE_INACCURATE (-7)
+        #define SCS_UNBOUNDED_INACCURATE (-6)
+        #define SCS_SIGINT (-5)
+        #define SCS_FAILED (-4)
+        #define SCS_INDETERMINATE (-3)
+        #define SCS_INFEASIBLE (-2) /* primal infeasible, dual unbounded   */
+        #define SCS_UNBOUNDED (-1)  /* primal unbounded, dual infeasible   */
+        #define SCS_UNFINISHED (0)  /* never returned, used as placeholder */
+        #define SCS_SOLVED (1)
+        #define SCS_SOLVED_INACCURATE (2)
+        if !(scs_sol.ret_val in [1,2])
+            error("Model not solved correctly.
+                   SCS termination status: $(scs_sol.ret_val)\n
+                   Info: $(scs_sol.info)")
+        elseif scs_sol.ret_val == 2
+            @warn "Maximum iterations reached. Set max_iters to increase accuracy"
+        end
+
+        return scs_sol.x, scs_sol.y[reverse_map], scs_sol.s[reverse_map]
+    end
+
+    #TODO: interface directly with other optimizer solvers
+
+    # If another optimizer is used, construct the problem with JuMP
+    # NOTE: this has a high computational overhead
     model = Model()
-    set_optimizer(model, optimizer_with_attributes(
-        SCS.Optimizer, "eps" => 1e-10, "max_iters" => 100000, "verbose" => 0))
+    set_optimizer(model, optimizer_factory)
+    # TODO: allow for attributes in general optimizer, e.g.
+    # optimizer_with_attributes(SCS.Optimizer, "eps" => 1e-6, etc...)
     @variable(model, x[1:n])
     @variable(model, s[1:m])
     @objective(model, Min, c'*x)
@@ -184,17 +233,14 @@ function solve_opt_problem(A, b, c, cone_prod, warm_start, optimizer_factory)
     if !(termination_status(model) in [MOI.OPTIMAL, MOI.ALMOST_OPTIMAL])
         error("Model not solved correctly.
                Termination status: $(termination_status(model))")
-    end
-    if termination_status(model) != MOI.OPTIMAL
-        @warn "Maximum iterations reached. Set 'max_iters' to increase accuracy"
+    elseif termination_status(model) == MOI.ALMOST_OPTIMAL
+        @warn "Maximum iterations reached. Set max_iters to increase accuracy"
     end
 
-    # primal_status(model)
-    # dual_status(model)
-    # TODO: for some reason this fails when b is a sparse vector?
-    #    Maybe interface directly with SCS?
-    y = isapprox(A'* dual.(con) + c, zeros(length(c)), atol=1e-6) ? dual.(con) : -dual.(con)
-    return value.(x), y, value.(s), sol
+
+    # Ensure dual has the correct sign
+    y = isapprox(A'* dual.(con) + c, zeros(length(c)), atol=1e-5) ? dual.(con) : -dual.(con)
+    return value.(x), y, value.(s)
 end
 
 
